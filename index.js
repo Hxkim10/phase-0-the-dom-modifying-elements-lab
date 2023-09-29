27736ef84e5a8b42f5b4adf99d231ff209ac8a2a
hxkim10 <abdelhakimtiabi@gmail.com>
2023-09-29 13:29:41 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Remove the main#main node
+const main = document.querySelector('#main');
+main.remove();
+
+// Create a new header with the correct text and id
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
